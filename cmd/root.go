3923c60b2239e27a866a7fce2ed09a92d95eeb2e
Jip J. Dekker <jip@dekker.li>
2016-03-16 21:46:14 +0100

    Adds description to the ponder rootcommand

--- cmd/root.go
+++ cmd/root.go
@@ -24,13 +24,11 @@ import (
 // This represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "ponder",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A managing tool for lilypond sheet music libraries",
+	Long: `Ponder is a tool to help manage your sheet music library.
+The main purpose is to help in the compilation of your lilypond files
+into both single files and a fully functioning song book. It also accepts
+other PDF files to be part of your song book.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
