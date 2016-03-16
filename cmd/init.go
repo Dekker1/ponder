2e7e4791b952c5c176112143b3737bb9756bfa9f
Jip J. Dekker <jip@dekker.li>
2016-03-16 21:39:18 +0100

    Adds description to the init command

--- cmd/init.go
+++ cmd/init.go
@@ -22,14 +22,14 @@ import (
 
 // initCmd represents the init command
 var initCmd = &cobra.Command{
-	Use:   "init",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Use:   "init [name]",
+	Short: "Initialize a Ponder Library",
+	Long: `Initialize (ponder init) will create a new library, with a ponder
+	settings file and corresponding git ignore file.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+  * If a name is provided, it will be created in the current directory;
+  * If no name is provided, the current directory will be assumed;
+Init will not use an existing directory with contents.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: Work your own magic here
 		fmt.Println("init called")
