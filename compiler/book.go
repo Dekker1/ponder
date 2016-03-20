6bddbd2470d4114f837dc30820610cbe2b9d7ab6
Jip J. Dekker <jip@dekker.li>
2016-03-20 16:19:27 +0100

    Adds placeholder MakeBook function

---
+++ compiler/book.go
@@ -0,0 +1,24 @@
+// Copyright © 2016 Jip J. Dekker <jip@dekker.li>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package compiler
+
+import "github.com/jjdekker/ponder/settings"
+
+// MakeBook will combine all scores into a single songbook
+// generated using LaTeX.
+func MakeBook(path string, opts *settings.Settings) {
+	// Everything needs to be compiled
+	CompileDir(path, opts)
+}
